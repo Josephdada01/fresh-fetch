4038df08b5c4c4b7575689cf2cbea102e3b5d353
Josephdada01 <ridwandada500@gmail.com>
2024-06-24 14:24:07 +0100

    worked on the create, update view for vendors

--- backend/product/urls.py
+++ backend/product/urls.py
@@ -1,7 +1,18 @@
-from django.urls import path
+from django.urls import path, include
 from product.views import ProductList, ProductDetail
+from rest_framework.routers import DefaultRouter
+from product.views import ProductViewSet
 
 urlpatterns = [
     path('api/v1/products/', ProductList.as_view(), name='product-list'),
     path('api/v1/products/<int:pk>/', ProductDetail.as_view(), name='product-detail'),
 ]
+
+# registering the viewset for vendor product creation and updating
+router = DefaultRouter()
+router.register(r'products', ProductViewSet, basename='product')
+
+urlpatterns += [
+    path('api/v1/', include(router.urls)),
+]
+
--- backend/product/views.py
+++ backend/product/views.py
@@ -1,15 +1,40 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
+from rest_framework.permissions import isAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from product.models import Product
 from product.serializers import ProductSerializer
+from users.models import User
+
+class isVendor(isAuthenticated):
+    """
+    Custom permission to allow only vendors to create, update, and delete products.
+    """
+    def has_permission(self, request, view):
+        """function that handle the permission"""
+        return super().has_permission(request, view) and request.user.is_vendor
 
 # Create your views here.
 class ProductList(generics.ListCreateAPIView):
+    """listing the views"""
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    """listing the detailviews"""
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+class ProductViewSet(viewsets.ModelViewset):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [isVendor]
+
+    def perform_create(self, serializer):
+        """vendor can perform the creation"""
+        serializer.save(user=self.request.user)
+
+    def perform_update(self, serializer):
+        """vendor can perform the update"""
+        serializer.save(user=self.request.user)
