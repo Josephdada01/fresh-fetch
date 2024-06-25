fc94baf1d4b627ae8413cdb404478cf49cda6237
Josephdada01 <ridwandada500@gmail.com>
2024-06-25 02:12:09 +0100

    debugged the api end points and ensure it is working now

--- backend/product/urls.py
+++ backend/product/urls.py
@@ -4,15 +4,22 @@ from rest_framework.routers import DefaultRouter
 from product.views import ProductViewSet
 
 urlpatterns = [
-    path('api/v1/products/', ProductList.as_view(), name='product-list'),
-    path('api/v1/products/<int:pk>/', ProductDetail.as_view(), name='product-detail'),
+    path('products/', ProductList.as_view(), name='product-list'),
+    path('products/<int:pk>/', ProductDetail.as_view(), name='product-detail'),
 ]
 
-# registering the viewset for vendor product creation and updating
 router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename='product')
 
 urlpatterns += [
-    path('api/v1/', include(router.urls)),
+    path('', include(router.urls)),
 ]
 
+"""
+router = DefaultRouter()
+router.register(r'products', ProductViewSet, basename='product')
+
+urlpatterns = [
+    path('api/v1/', include(router.urls)),
+]
+"""
--- backend/product/views.py
+++ backend/product/views.py
@@ -6,14 +6,16 @@ from rest_framework.views import APIView
 from product.models import Product
 from product.serializers import ProductSerializer
 from users.models import User
-
+from product.permissions import IsVendor
+"""
 class isVendor(IsAuthenticated):
-    """
+    
     Custom permission to allow only vendors to create, update, and delete products.
-    """
+    
     def has_permission(self, request, view):
-        """function that handle the permission"""
+        function that handle the permission
         return super().has_permission(request, view) and request.user.is_vendor
+"""
 
 # Create your views here.
 class ProductList(generics.ListCreateAPIView):
@@ -29,7 +31,7 @@ class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [isVendor]
+    permission_classes = [IsVendor]
 
     def perform_create(self, serializer):
         """vendor can perform the creation"""
