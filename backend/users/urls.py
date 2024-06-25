9a8639a9ee6ffd7ad4a2c12b77c213ce6532cedd
Solomon <solomonuche@gmail.com>
2024-06-25 18:22:45 +0100

    update urls to include vendor list view

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path, include
 from .views import VendorListView
-from django.conf.urls.static import static
-from django.conf import settings
 
 urlpatterns = [
     path('users/', include('dj_rest_auth.urls')),
