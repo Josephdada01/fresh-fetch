bcf56b6c228afd3964abcc4476bc808697c1ef76
Josephdada01 <ridwandada500@gmail.com>
2024-06-29 15:07:37 +0100

    ammend the  order urls to work with the order models

--- backend/order/urls.py
+++ backend/order/urls.py
@@ -3,5 +3,5 @@ from order.views import OrderList, OrderDetail
 
 urlpatterns = [
     path('orders/', OrderList.as_view(), name='order-list'),
-    path('orders/<int:pk>/', OrderDetail.as_view(), name='order-detail'),
+    path('orders/<uuid:pk>/', OrderDetail.as_view(), name='order-detail'),
 ]
