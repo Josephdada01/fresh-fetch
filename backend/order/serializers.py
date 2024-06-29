1a3a4e2c54a4c292d2da3befd7ffe885fa84d957
Josephdada01 <ridwandada500@gmail.com>
2024-06-29 12:22:45 +0100

    fixed a bug

--- backend/order/serializers.py
+++ backend/order/serializers.py
@@ -5,7 +5,7 @@ from product.serializers import ProductSerializer, Product
 class OrderItemSerializer(serializers.ModelSerializer):
     """serializers for the orderItems class"""
     #product = ProductSerializer()
-    product_id = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all(), source='product.id')
+    id = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all(), source='product.id')
    
     class Meta:
         model = OrderItems
