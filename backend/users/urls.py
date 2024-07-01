43b60c988c8a1e7b77691d75e4e1e5238028cbbd
Solomon <solomonuche@gmail.com>
2024-07-01 13:06:26 +0100

    get and edit vendor orders implemented

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
-from .views import VendorListView
+from .views import VendorListView, VendorOrderListView, VendorOrderUpdateView
 
 urlpatterns = [
     path('users/', include('dj_rest_auth.urls')),
     path('users/register/', include('dj_rest_auth.registration.urls')),
     path('vendors/', VendorListView.as_view(), name='vendors-list'),
+    path('vendors/orders/', VendorOrderListView.as_view(), name='vendors-orders'),
+    path('vendors/orders/<order_id>/', VendorOrderUpdateView.as_view(), name='vendors-orders-update'),
 ]
